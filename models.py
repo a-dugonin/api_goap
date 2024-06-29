88b9c6ee15ec276bd055872a49794b2d7ff39752
a-dugonin <a_dugonin@mail.ru>
2024-06-29 16:58:16 +0300

    Добавление requirements.txt

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 
-from peewee import SqliteDatabase, Model, AutoField, CharField, IntegerField, DateTimeField
+from peewee import (
+    SqliteDatabase,
+    Model,
+    AutoField,
+    CharField,
+    IntegerField,
+    DateTimeField,
+)
 from pydantic import BaseModel
 
 db = SqliteDatabase("database.db")
@@ -12,7 +19,8 @@ class DefaultModel(Model):
 
 
 class Tickets(DefaultModel):
-    """ Модель описывающая таблицу tickets в БД """
+    """Модель описывающая таблицу tickets в БД"""
+
     request_id = AutoField()
     ne_name = CharField()
     status = IntegerField()
@@ -20,29 +28,33 @@ class Tickets(DefaultModel):
 
 
 class Alarms(DefaultModel):
-    """ Модель описывающая таблицу alarms в БД """
+    """Модель описывающая таблицу alarms в БД"""
+
     notification_id = AutoField()
     ne_name = CharField()
     eventtime = DateTimeField(default=datetime.now())
 
 
 def create_models():
-    """ Метод для создания таблиц в БД """
+    """Метод для создания таблиц в БД"""
     db.create_tables(DefaultModel.__subclasses__())
 
 
 class TicketCreate(BaseModel):
-    """ Pydantic модель для валидации данных при создании записи в таблице tickets """
+    """Pydantic модель для валидации данных при создании записи в таблице tickets"""
+
     ne_name: str
     status: int
 
 
 class TicketUpdate(BaseModel):
-    """ Pydantic модель для валидации данных при обновлении статуса записи в таблице tickets """
+    """Pydantic модель для валидации данных при обновлении статуса записи в таблице tickets"""
+
     request_id: int
     status: int
 
 
 class AlarmCreate(BaseModel):
-    """ Pydantic модель для валидации данных при создании записи в таблице alarms """
+    """Pydantic модель для валидации данных при создании записи в таблице alarms"""
+
     ne_name: str
